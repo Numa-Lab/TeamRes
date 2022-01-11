7a402286c8cbae6e1bbe5be352b275f979568488
Bun133 <bun133.work@gmail.com>
2022-01-11 17:22:27 +0900

    add resources/main

---
+++ main/src/main/java/net/kunmc/lab/teamres/Teamres.kt
@@ -0,0 +1,13 @@
+package net.kunmc.lab.teamres
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Teamres : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
